8ea4fa59af88242f64ca76c5f9ceafa8a7946518
Hernando Brenes <hbrenes@mergeit.net>
2018-05-10 19:45:44 -0400

    Explicación de Constructores y Sobrecargas

--- TestApp/TestApp/Player.cs
+++ TestApp/TestApp/Player.cs
@@ -8,6 +8,8 @@ namespace TestApp
 {
     class Player
     {
+
+        #region Propiedades
         //Propiedades (variables) - Estado
         private string name;
         public string Name
@@ -24,18 +26,15 @@ namespace TestApp
         }
 
         //private int highScore;
-        public int HighScore {
-
-            get { return this.HighScore; }
-
-        }
+        public int HighScore { get; set; }
         public int HeathPoints { get; set; }
         public int Speed { get; set; }
         public Stance PlayerStance { get; set; }
 
+        #endregion
 
-        //Métodos (funciones) - Comportamiento
-       public void Walk()
+        #region Métodos (funciones) - Comportamiento
+        public void Walk()
         {
             this.Speed = 5;
             this.PlayerStance = Stance.Standing;
@@ -54,13 +53,35 @@ namespace TestApp
 
         }
 
-        public int TakeDamage(int damageLevel)
+
+
+        public int Attack(int damageLevel)
         {
+            Random rnd = new Random();
+            int randomNum = rnd.Next();
+
+            if (randomNum == 3)
+            {
+                return this.Attack(damageLevel, true);
+            }
+            else
+                return this.Attack(damageLevel, false);
+        }
+
+        private int Attack(int damageLevel, bool isMiss)
+        {
+            if (isMiss)
+            {
+                Console.WriteLine("Player miss!");
+                return this.HeathPoints;
+            }
+            
+
             Console.WriteLine("Player took {0} of damage", damageLevel);
             if (damageLevel >= this.HeathPoints)
             {
                 this.HeathPoints = 0;
-                
+
                 this.Die();
                 return 0;
             }
@@ -68,9 +89,19 @@ namespace TestApp
             {
                 this.HeathPoints -= damageLevel;
             }
-                
+
             return this.HeathPoints;
+
+
+            
         }
+
+        public int Attack(double damageLevel)
+        {
+            return 5;
+        }
+
+        #endregion
 
         //Constructor
         //Tiene el nombre de la clase
@@ -78,6 +109,21 @@ namespace TestApp
         //puede tener cero o más parámetros
         //usualmente es public
         public Player()
+        {
+            this.HeathPoints = 100;
+        }
+
+        public Player(string name) : this() //this() permite llamar una sobrecarga del constructor antes de correr este constructor.
+        {
+            this.name = name;
+        }
+
+        public Player(string name, int score) : this(name)
+        {
+            this.HighScore = score;
+        }
+
+        ~Player()
         {
 
         }
--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -15,23 +15,27 @@ namespace TestApp
 
             //el tipo de dato de la clase
             //una variable que haga referencia al objeto
-            Player player1 = new Player();
-            player1.Name = "Hernando";
+            Player player1 = new Player("Hernando", 10);
+           
+
+            Console.WriteLine("Player name is {0} and has {1} HP.", player1.Name, player1.HeathPoints);
             Console.WriteLine("Player name is " + player1.Name);
 
 
 
             player1.Walk();
-            player1.HeathPoints = 100;
 
-            Console.WriteLine("Player 1 took damage, curren hp is {0}",player1.TakeDamage(70));
-            Console.WriteLine("Player 1 took damage, curren hp is {0}", player1.TakeDamage(10));
-            player1.TakeDamage(5);
+            //TODO: Arreglar esto, debe ser parte del constructor.
+            player1.HeathPoints = 1000;
+
+            Console.WriteLine("Player 1 took damage, curren hp is {0}",player1.Attack(70));
+            Console.WriteLine("Player 1 took damage, curren hp is {0}", player1.Attack(10));
+            player1.Attack(5);
             Console.WriteLine("Current HP is {0}", player1.HeathPoints);
 
-            player1.TakeDamage(400);
+            player1.Attack(400);
 
-            
+            player1.Attack(10);
 
             
               
