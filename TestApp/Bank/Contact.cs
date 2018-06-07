b1a7241fab13b35c15e331cdb23edc85c329531c
Hernando Brenes <hbrenes@mergeit.net>
2018-06-07 19:54:30 -0400

    Arrelgos, Listas, LINQ

--- TestApp/Bank/Contact.cs
+++ TestApp/Bank/Contact.cs
@@ -13,11 +13,31 @@ namespace Bank
         public string EmailAddress { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
-        public BankAccount Account { get; set; }
+        protected List<BankAccount> accounts;
+
+        //public BankAccount Account { get; set; }
 
         public double Balance
         {
-            get { return Account.Balance; }
+            get
+            {
+
+                //var res = (from c in accounts
+                //          where c.Balance > 1000
+                //          select c)
+
+
+                return accounts.Sum(c => c.Balance);
+
+                //double balance = 0;
+
+                //foreach (var acc in accounts)
+                //{
+                //    balance += acc.Balance;
+                //}
+
+                //return balance;
+            }
         }
 
         public Contact(string id, string name, string email, string address, string phoneNumber, string accountId, double initialBalance)
@@ -28,10 +48,37 @@ namespace Bank
             this.Address = address;
             this.PhoneNumber = phoneNumber;
 
-            this.Account = new BankAccount(accountId, initialBalance, BankAccountType.Savings);
+            this.accounts = new List<BankAccount>();
+            CreateBankAccount(accountId, initialBalance, BankAccountType.Savings);
         }
 
+        //HACK: este no es el mejor lugar para esto.
+        public void CreateBankAccount(string id, double initialBalance, BankAccountType type)
+        {
+            this.accounts.Add(new BankAccount(id, initialBalance, type));
 
+        }
 
+        public void Deposit(string id, double amount)
+        {
+            BankAccount acc = this.accounts.Where(c => c.AccountNumber == id).FirstOrDefault();
+
+            if (acc != null)
+            {
+                acc.Deposit(amount);
+            }
+        }
+
+        public bool Withdrawl(string id, double amount)
+        {
+            BankAccount acc = this.accounts.Where(c => c.AccountNumber == id).FirstOrDefault();
+
+            if (acc != null)
+            {
+                return acc.Withdrawl(amount);
+            }
+            else
+                return false;
+        }
     }
 }
--- TestApp/Bank/Program.cs
+++ TestApp/Bank/Program.cs
@@ -10,18 +10,47 @@ namespace Bank
     {
         static void Main(string[] args)
         {
+            
+
+            //string[] arr = new string[3]; //{5, 6, 8,10,50 };
+
+            //arr[0] = "Hola";
+            //arr[1] = "Como";
+            //arr[2] = "Estas";
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine($"Position {i}: {arr[i]}");
+            //}
+
+            //List<string> names = new List<string>();
+            //names.Add("Juan");
+            //names.Add("Pedro");
+
+            //foreach (var name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+
+
+
             //Refactoring
 
-            Contact contacto;
-            contacto = new Contact("0012222222", "Hernando Brenes", "hbrenes@gmail.com", "Frank Felix Miranda 32", "809-729-2899", "123456789", 100);
+     
+            var contacto = new Contact("0012222222", "Hernando Brenes", "hbrenes@gmail.com", "Frank Felix Miranda 32", "809-729-2899", "123456789", 100);
             PrintBalance(contacto);
+
+            
+
+
             //deposit 500
-            contacto.Account.Deposit(500);
+            contacto.Deposit("123456789", 500);
 
             PrintBalance(contacto);
 
             //withdrawl 700
-            bool result = contacto.Account.Withdrawl(700);
+            bool result = contacto.Withdrawl("123456789", 700);
 
             if (result == false)
             {
@@ -30,24 +59,32 @@ namespace Bank
 
             PrintBalance(contacto);
 
-            result = contacto.Account.Withdrawl(400);
+            result = contacto.Withdrawl("123456789", 400);
 
             PrintBalance(contacto);
 
-            switch (contacto.Account.Type)
-            {
-                case BankAccountType.Savings:
-                case BankAccountType.SuperSuperSavings:
-                    Console.WriteLine("This is a savings account.");
-         
-                
-                    //Console.WriteLine("This is a super super savings account.");
-                    break;
-                case BankAccountType.Checking:
-                    break;
-                default:
-                    break;
-            }
+            contacto.CreateBankAccount("2222", 100, BankAccountType.Checking);
+
+            PrintBalance(contacto);
+
+            contacto.Deposit("2222", 500);
+
+            PrintBalance(contacto);
+
+            //switch (contacto.Account.Type)
+            //{
+            //    case BankAccountType.Savings:
+            //    case BankAccountType.SuperSuperSavings:
+            //        Console.WriteLine("This is a savings account.");
+
+
+            //        //Console.WriteLine("This is a super super savings account.");
+            //        break;
+            //    case BankAccountType.Checking:
+            //        break;
+            //    default:
+            //        break;
+            //}
 
             Console.Read();
         }
