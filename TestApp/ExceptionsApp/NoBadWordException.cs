c5fc8c2f715a60f39089e9e776166b71ef7ded31
hbrenes@gmail.com <hbrenes@gmail.com>
2018-06-19 19:37:22 -0400

    Exceptions

---
+++ TestApp/ExceptionsApp/NoBadWordException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsApp
+{
+    class NoBadWordException : ApplicationException
+    {
+        public NoBadWordException()
+        {
+        }
+
+        public NoBadWordException(string message) : base(message)
+        {
+        }
+
+        public NoBadWordException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NoBadWordException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ TestApp/ExceptionsApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+                Console.Write("Escribe tu nombre: ");
+
+                string name = Console.ReadLine();
+
+                if (name.ToLower() == "fuck")
+                {
+                    throw new NoBadWordException("No es un nombre valido.");
+                }
+
+                Console.WriteLine("Escribe un numero: ");
+
+                int x = Int32.Parse(Console.ReadLine());
+
+
+
+                Console.WriteLine("Escribe otro numero: ");
+
+                int y = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine($"La division es: {x / y}");
+            }
+            catch (NoBadWordException ex)
+            {
+                Console.WriteLine("Pana, no se puede decir malas palabras");
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                Console.WriteLine("Pana, no se puede dividir por cero. Trata again");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Hubo un error: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Yo siempre corro.");
+            }
+            
+            
+
+            Console.Read();
+        }
+    }
+}
